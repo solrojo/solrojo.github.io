195ae2e5645303a26998f689d12ada725dda76cc
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-01 22:18:09 -0600

    refactor script

--- utils/buildDataJson/index.js
+++ utils/buildDataJson/index.js
@@ -2,43 +2,34 @@ const fs = require('fs');
 const minimist = require('minimist');
 
 const argv = minimist(process.argv);
-let phrases = [];
 
-try {
-  console.log('Reading a source file...')
+const buildObj = (arr) => ({
+  phrase: arr[0].trim(),
+  ru: arr[1].trim()
+});
 
-  phrases = fs.readFileSync(argv.f, 'utf8')
-    .split('\n')
-    .filter(item => Boolean(item))
-    .map(item => {
-      const found = item.match(/\[.+\]/g);
-      return found
-        ? found.join().replace(/\[|\]/g, '').split(' ').map(el => item.replace(found, el))
-        : item
-    })
-    .flat()
-    .map((item) => {
-      const arr = item.split(':');
-      return {
-        phrase: arr[0].trim(),
-        ru: arr[1].trim()
-      };
-    });
+const phrases = fs.readFileSync(argv.f, 'utf8')
+  .split('\n')
+  .filter(item => Boolean(item))
+  .map(item => {
+    const found = item.match(/\[.+\]/g);
+    return found
+      ? (
+        found[0]
+          .replace(/\[|\]/g, '')
+          .split(' ')
+          .map(el => buildObj(
+            item.replace(found, el).split(':')
+          ))
+      )
+      : buildObj(item.split(':'));
+  })
+  .flat();
 
-    if (!(phrases[0].hasOwnProperty('phrase') && phrases[0].hasOwnProperty('ru'))) {
-      throw new Error('Incorrect phrases object type');
-    }
-} catch (err) {
-  console.error(err);
+if (!(phrases[0].hasOwnProperty('phrase') && phrases[0].hasOwnProperty('ru'))) {
+  throw new Error('Incorrect phrases object type');
 }
 
-console.log('Source file successfully read.')
+fs.writeFileSync(argv.o, JSON.stringify(phrases));
 
-try {
-  console.log('Writing to output file...')
-  fs.writeFileSync(argv.o, JSON.stringify(phrases));
-} catch (err) {
-  console.error(err);
-}
-
-console.log('Success! Exit.');
+console.log('\x1b[42m', 'Done ');

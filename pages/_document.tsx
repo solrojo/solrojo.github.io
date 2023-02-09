5b02dc909e7e63552addb88b7eae793b416b4f94
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-09 03:23:40 -0600

    bg image update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,7 +10,9 @@ export default () => (
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="preload" as="image" href="/img/acid-bg.png"></link>
+
+      <link rel="preload" as="image" href="/img/bg-mobile.png" media="(max-width: 480px)" />
+      <link rel="preload" as="image" href="/img/bg-desktop.png" media="(min-width: 481px)" />
 
       <Script id="google-analytics">
         {`

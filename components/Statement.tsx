31d4183b9c48528c2c7dfcf3c9f1d702890654dc
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-09 02:14:33 -0600

    redesign

--- components/Statement.tsx
+++ components/Statement.tsx
@@ -4,7 +4,7 @@ import styles from '@/styles/Statement.module.css'
 export default () => (
   <div className={styles.container}>
     <div className={styles.text}>
-      <p className={styles.element}>Stand with Ukraine</p>
+      <p className={styles.element}>Stand with Ukraine 🇺🇦</p>
       <ActionLinkExternal href="https://u24.gov.ua">u24.gov.ua</ActionLinkExternal>
     </div>
 

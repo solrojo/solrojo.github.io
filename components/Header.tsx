b76a8b7c5f3654d354ee2b5791f2fd2ee33a9c05
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-09 00:35:20 -0600

    redesign

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,10 @@
-import { ReactNode } from 'react'
-import PropTypes from 'prop-types'
 import Logo from '@/components/Logo'
 import Menu from '@/components/Menu'
 import styles from '@/styles/Header.module.css'
 
-type Props = {
-  children?: ReactNode
-}
-
-const Header = ({ children }: Props) => (
+export default () => (
   <div className={styles.container}>
     <Logo />
-    { children }
     <Menu />
   </div>
 )
-
-Header.propTypes = {
-  children: PropTypes.node
-}
-
-export default Header

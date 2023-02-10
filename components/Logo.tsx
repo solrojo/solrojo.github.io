6050a93fba417eab8f119317bb59b29f06c59fb1
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-09 21:37:48 -0600

    add logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,4 +1,14 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from '@/styles/Logo.module.css'
 
-export default () => <Link href="/" className={styles.element}>kda.name</Link>
+export default () => (
+  <Link href="/" className={styles.container}>
+    <Image
+      src="/img/kda.name.svg"
+      alt="kda.name"
+      width={150}
+      height={20}
+    />
+  </Link>
+)

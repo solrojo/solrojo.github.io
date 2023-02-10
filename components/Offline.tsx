300267e9cd145a5f8ad2b2d30bc6ab41d12f6eae
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-09 18:38:20 -0600

    add offline message

---
+++ components/Offline.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef } from 'react'
+import { CSSTransition } from 'react-transition-group'
+import styles from '@/styles/Offline.module.css'
+
+export default () => {
+  const [iVisible, setVisibility] = useState(false)
+  const nodeRef = useRef(null);
+
+  const hadleOffline = () => setVisibility(true)
+  const hadleOnline = () => setVisibility(false)
+
+  useEffect(() => {
+    window.addEventListener('offline', hadleOffline);
+    return () => window.removeEventListener('offline', hadleOffline)
+  }, [hadleOffline]);
+
+  useEffect(() => {
+    window.addEventListener('online', hadleOnline);
+    return () => window.removeEventListener('online', hadleOnline)
+  }, [hadleOnline]);
+
+
+  return (
+    <CSSTransition
+      nodeRef={nodeRef}
+      in={iVisible}
+      timeout={300}
+      classNames={{
+        enter: styles.enter,
+        enterActive: styles.enterActive,
+        enterDone: styles.enterDone,
+        exit: styles.exit,
+        exitActive: styles.exitActive
+      }}
+      unmountOnExit
+    >
+      <div ref={nodeRef} className={styles.container}>
+        Looks like we're offline
+      </div>
+    </CSSTransition>
+  )
+}

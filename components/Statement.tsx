dd9008b3425e01657decd085c8d1c96819694b48
Dmitrii Korchemkin <chvrkch@icloud.com>
2023-02-10 23:48:11 -0600

    restyling

--- components/Statement.tsx
+++ components/Statement.tsx
@@ -5,7 +5,7 @@ export default () => (
   <div className={styles.container}>
     <div className={styles.text}>
       <p className={styles.element}>Stand with Ukraine 🇺🇦</p>
-      <ActionLinkExternal href="https://u24.gov.ua">u24.gov.ua</ActionLinkExternal>
+      <ActionLinkExternal href="https://u24.gov.ua" withBg>u24.gov.ua</ActionLinkExternal>
     </div>
 
     <div className={styles.image}></div>
